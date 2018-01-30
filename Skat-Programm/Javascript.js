508a92432628d53ae0e72bfa4af6f8e6b9c36986
Per Bernhardt <per.bernhardt@chefkoch.de>
2018-01-30 14:44:12 +0100

    Auf jquery umgestellt und das Löschen von einzelnen Einträgen ermöglicht

--- Skat-Programm/Javascript.js
+++ Skat-Programm/Javascript.js
@@ -1,35 +1,45 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-const input = document.getElementById('item');
+const form = $('form');
+const ul = $('ul');
+const button = $('.delete-all-button');
+const input = form.find('input');
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+const removeItemController = function(offset) {
+    itemsArray = itemsArray.filter(function(item, index) {
+        return index !== offset;
+    });
+    updateApp();
+};
 
-const liMaker = (text) => {
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
-}
-
-form.addEventListener('submit', function (e) {
+const addItemController = function (e) {
     e.preventDefault();
 
-    itemsArray.push(input.value);
+    itemsArray.push(input.val());
+    updateApp();
+    input.val('');
+};
+
+const removeAllController = function () {
+    itemsArray = [];
+    updateApp();
+};
+
+const updateApp = () => {
+    ul.html('');
+
+    itemsArray.forEach((text, offset) => {
+        ul.append('<li>' + text + ' <button>Diesen Eintrag löschen</button></li>');
+        ul.find('li:last button').click((e) => { removeItemController(offset); });
+    });
+
+    // Liga-Tabelle ausgeben
+
+    $('.anzahl-ergebnisse').html('Bislang wurden ' + itemsArray.length + ' Ergebnisse erfasst.');
+
     localStorage.setItem('items', JSON.stringify(itemsArray));
-    liMaker(input.value);
-    input.value = "";
-});
+}
 
-data.forEach(item => {
-    liMaker(item);
-});
+updateApp();
 
-button.addEventListener('click', function () {
-    localStorage.getItem('item');
-    localStorage.removeItem('item')
-    while (ul.firstChild); {
-        ul.removeChild(ul.firstChild);
-    }
-});
+form.submit(addItemController);
+button.click(removeAllController);
